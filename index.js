5a4f164a1e7fba2b56bdb216b13f9418201ff46e
Prof. Daniel Cornejo <ryu51474@gmail.com>
2022-08-29 14:22:44 -0400

    se elimina servicio simpsons de prueba por api fuera de servicio. se corrige el servicio de notas por estudiantes no existentes. se aplica prettier pero aun no me convence el estilo

--- index.js
+++ index.js
@@ -1,143 +1,159 @@
+const { Client, LocalAuth, MessageMedia, Buttons } = require("whatsapp-web.js");
+const codigoqr = require("qrcode-terminal");
+const fs = require("fs");
+const clever = require("cleverbot-free");
+const fetch = require("isomorphic-fetch");
 
-const { Client, LocalAuth, MessageMedia } = require('whatsapp-web.js');
-const codigoqr = require('qrcode-terminal');
-const  fs  = require('fs');
-const clever =require('cleverbot-free');
-const fetch = require('isomorphic-fetch');
-const jsdom = require('jsdom')
-const {JSDOM} = jsdom;
-
-//nueva forma de autenticarse. 
+//nueva forma de autenticarse.
 //ya no se necesitan archivos de sesiones
 // porque los guarda localmente
 cliente = new Client({
-    authStrategy: new LocalAuth(),
-    puppeteer: { headless: true }
+  authStrategy: new LocalAuth(),
+  puppeteer: { headless: true },
 });
 
 //aqui se genera el codigo qr
 //SOLO si no estan los datos guardados en
-cliente.on('qr',qr => {
-    console.log('no habia sesion iniciada');
-    codigoqr.generate(qr,{small:true});
-    console.log('se inicia sesion, por favor escanee el qr de arriba');
+cliente.on("qr", (qr) => {
+  console.log("no habia sesion iniciada");
+  codigoqr.generate(qr, { small: true });
+  console.log("se inicia sesion, por favor escanee el qr de arriba");
 });
-cliente.on('ready',()=>{
-    console.log('cliente inicializado, ya se puede operar');
-})
-
-cliente.on('auth_failure', (errorAutenticacion) => {
-     console.log('fallo el inicio de sesion porque: ',errorAutenticacion)
-    // connectionLost()
+cliente.on("ready", () => {
+  console.log("cliente inicializado, ya se puede operar");
 });
 
-
+cliente.on("auth_failure", (errorAutenticacion) => {
+  console.log("fallo el inicio de sesion porque: ", errorAutenticacion);
+  // connectionLost()
+});
 
 //para recibir y escuchar los mensajes entrantes
-cliente.on('message',mensajeEntrante => {
-    let cuerpoMensaje = mensajeEntrante.body;
-    console.log(mensajeEntrante.body);
-    let nombreNotificacion = mensajeEntrante._data.notifyName;
-    console.log(nombreNotificacion);
-    let numeroEmisor=mensajeEntrante.from;
-    console.log(mensajeEntrante.from);
-    console.log(mensajeEntrante.to);
+cliente.on("message", (mensajeEntrante) => {
+  let cuerpoMensaje = mensajeEntrante.body;
+  console.log(mensajeEntrante.body);
+  let nombreNotificacion = mensajeEntrante._data.notifyName;
+  console.log(nombreNotificacion);
+  let numeroEmisor = mensajeEntrante.from;
+  console.log(mensajeEntrante.from);
+  console.log(mensajeEntrante.to);
 
-    var ahora = new Date() //se ha subido aqui, sacado del primer if porque solo debe responder el bot si es muy tarde
-    
-    
-    if(mensajeEntrante.body.toLowerCase().search(/hola/)>=0){//si el mensaje viene con la palabra hola responde un saludo al azar
+  var ahora = new Date(); //se ha subido aqui, sacado del primer if porque solo debe responder el bot si es muy tarde
 
-        var arrayRespuestas=[
-            `estas bien?, un gusto saludarte ${nombreNotificacion}`,
-            `son las ${ahora.getHours()}:${ahora.getMinutes()} en este momento, en serio me escribes a esta hora ${nombreNotificacion}?`,
-            `palabras, siempre palabras. por que no me dices de una vez que quieres ${nombreNotificacion}?`,
-            `${nombreNotificacion}, podrias mejorar lo que me dices`,
-            `primero el mensaje de saludos, bien ${nombreNotificacion}`
-        ]
-        mensajeRespuestaSaludoAzar=arrayRespuestas[Math.floor(Math.random()*arrayRespuestas.length)]
-        cliente.sendMessage(mensajeEntrante.from,mensajeRespuestaSaludoAzar);
-        console.log(mensajeRespuestaSaludoAzar);
-        
-    }else if(cuerpoMensaje.toLowerCase().search(/nota/)>=0){//si en el mensaje existe la palabra nota da instrucciones para recibir notas
-        cliente.sendMessage(numeroEmisor,`${nombreNotificacion}, si deseas saber notas debes de ahora ingresar solo tu rut, sin puntos ni guión, en caso de terminar en k reemplácelo con un 1, ej: el rut 12.345.678-k se escribe 123456781. SI NO LO HACE CORRECTAMENTE SU PETICION SERA ANULADA E IGNORADA (Puede que se responda con cualquier cosa absurda)`);
-    }else if(!isNaN(cuerpoMensaje)){//si escribe un numero se toma como un rut y se analiza si se puede sacar las notas
-        var RUT=cuerpoMensaje.replace(/[\.,-]/g,'')//no tiene sentido el    .replace(/k/gi,'1')
-        cliente.sendMessage(numeroEmisor,'Espere un momento mientras reviso sus datos. si no respondo en un tiempo prudencial puede volver a intentarlo enviando nuevamente su rut');
-        const urlApiNotas='https://script.google.com/macros/s/AKfycbyYYD23WAZ2_XBfRBgbeX4R5XqCwbfaPvrYkKQ38Dh7J3oPGKKQqv-3l8m8XxR_OaEKoQ/exec?sdata='
-        fetch(urlApiNotas+cuerpoMensaje)
-            .then(respuestaApiNotas=>{
-                return respuestaApiNotas;
-            })
-            .then(direccionObtenida=>{
-                fetch(direccionObtenida.url)
-                    .then(respuestadeDireccion=>{
-                        return respuestadeDireccion.text();
-                    })
-                    .then(respuestaTextodeDireccion=>{//recibo el string html
-                        var nombreArchivomedia=`informe notas de fisica solicitado por ${nombreNotificacion} al ${ahora.getDate()}-${ahora.getMonth()}-${ahora.getFullYear()}.html`
-                        var pathFileNombrearchivo=`./informes/${nombreArchivomedia}`;
-                        //escribo el archivo localmente
-                        new MessageMedia(fs.writeFile(pathFileNombrearchivo
-                                                      ,respuestaTextodeDireccion
-                                                      ,errorescrituraArchivo=>{
-                                                        console.log(errorescrituraArchivo)
-                                                       }
-                                                      ));
-                        //envio el archivo del informe dandole un tiempo de espera
-                        setTimeout(async()=>{
-                            var archivomedia=MessageMedia.fromFilePath(`./informes/${nombreArchivomedia}`)
-                            await cliente.sendMessage(numeroEmisor,archivomedia);
-                            //ahora borro el achivo generado
-                            await fs.unlinkSync(pathFileNombrearchivo);
-                        },10000);
-                        
-                    })
-                    .catch(errorDireccionObtenida=>{
-                        console.log('error de direccion obtenida url porque: '+errorDireccionObtenida)
-                    });
-                
-            })
-            .catch(errorApiNotas=>{
-                console.log('error en la api de notas porque: '+errorApiNotas);
-            });
-        //cliente.sendMessage(numeroEmisor,apirespuestafinal.toString());
-    }else if(cuerpoMensaje.toLowerCase().search(/boton/)>=0){
-        cliente.sendMessage(numeroEmisor,'boton')
-    }else if(cuerpoMensaje.toLowerCase().search(/simpson/)>=0){
-        const urlAPI='https://thesimpsonsquoteapi.glitch.me/quotes?character=homer';
-        const urlApIEsp='https://los-simpsons-quotes.herokuapp.com/v1/quotes'
-        fetch(urlApIEsp)
-                .then(APIpromiseresponse=>{console.log('aqui');return APIpromiseresponse.json()})
-                .then( respuestaObjetoAPI=>{
-                    console.log(respuestaObjetoAPI[0]);
-                    //await apirespuestafinal.push(respuestaObjetoAPI[0].quote);
-                    console.log(respuestaObjetoAPI[0].quote);
-                    console.log(respuestaObjetoAPI[0].author);
-                    cliente.sendMessage(numeroEmisor,respuestaObjetoAPI[0].quote);
-                    cliente.sendMessage(numeroEmisor,'Atte: '+respuestaObjetoAPI[0].author);
-                })
-                .catch(respuestaError=>{
-                    console.log(respuestaError)
-                });
-    }
-    else{
-        //mensajeCleverbot=clever(mensajeEntrante.body.toLowerCase()).then(respuestaclever);
-        //console.log(mensajeCleverbot);
-        //cliente.sendMessage(mensajeEntrante.from,"no dijiste hola");
+  if (mensajeEntrante.body.toLowerCase().search(/hola/) >= 0) {
+    //si el mensaje viene con la palabra hola responde un saludo al azar
 
-        /**contesta cleverbot */
-        clever(cuerpoMensaje).then(async respuestacleverBot=>{
-            await console.log('respuesta cleverbot: '+ respuestacleverBot);
-            cliente.sendMessage(numeroEmisor,respuestacleverBot)
-        })
-        .catch(errorCleverbot=>{
-            console.log(errorCleverbot);
-            cliente.sendMessage(numeroEmisor,`Por el momento tengo problemas para responder. escribeme mas tarde ${nombreNotificacion}`);
-        });
-    }
+    var arrayRespuestas = [
+      `estas bien?, un gusto saludarte ${nombreNotificacion}`,
+      `son las ${ahora.getHours()}:${ahora.getMinutes()} en este momento, en serio me escribes a esta hora ${nombreNotificacion}?`,
+      `palabras, siempre palabras. por que no me dices de una vez que quieres ${nombreNotificacion}?`,
+      `${nombreNotificacion}, podrias mejorar lo que me dices`,
+      `primero el mensaje de saludos, bien ${nombreNotificacion}`,
+    ];
+    mensajeRespuestaSaludoAzar =
+      arrayRespuestas[Math.floor(Math.random() * arrayRespuestas.length)];
+    cliente.sendMessage(mensajeEntrante.from, mensajeRespuestaSaludoAzar);
+    console.log(mensajeRespuestaSaludoAzar);
+  } else if (cuerpoMensaje.toLowerCase().search(/nota/) >= 0) {
+    //si en el mensaje existe la palabra nota da instrucciones para recibir notas
+    cliente.sendMessage(
+      numeroEmisor,
+      `${nombreNotificacion}, si deseas saber notas debes de ahora ingresar solo tu rut, sin puntos ni guión, en caso de terminar en k reemplácelo con un 1, ej: el rut 12.345.678-k se escribe 123456781. SI NO LO HACE CORRECTAMENTE SU PETICION SERA ANULADA E IGNORADA (Puede que se responda con cualquier cosa absurda)`
+    );
+  } else if (!isNaN(cuerpoMensaje)) {
+    //si escribe un numero se toma como un rut y se analiza si se puede sacar las notas
+    var RUT = cuerpoMensaje.replace(/[\.,-]/g, ""); //no tiene sentido el    .replace(/k/gi,'1')
+    cliente.sendMessage(
+      numeroEmisor,
+      "Espere un momento mientras reviso sus datos."
+    );
+    const urlApiNotas =
+      "https://script.google.com/macros/s/AKfycbyYYD23WAZ2_XBfRBgbeX4R5XqCwbfaPvrYkKQ38Dh7J3oPGKKQqv-3l8m8XxR_OaEKoQ/exec?sdata=";
+    fetch(urlApiNotas + cuerpoMensaje)
+      .then((respuestaApiNotas) => {
+        return respuestaApiNotas;
+      })
+      .then((direccionObtenida) => {
+        fetch(direccionObtenida.url)
+          .then((respuestadeDireccion) => {
+            return respuestadeDireccion.text();
+          })
+          .then((respuestaTextodeDireccion) => {
+            //recibo el string html
+            if (
+              respuestaTextodeDireccion !== "Estudiante no existe, reintente"
+            ) {
+              var nombreArchivomedia = `informe notas de fisica solicitado por ${nombreNotificacion} al ${ahora.getDate()}-${ahora.getMonth()}-${ahora.getFullYear()}.html`;
+              var pathFileNombrearchivo = `./informes/${nombreArchivomedia}`;
+              //escribo el archivo localmente
+              new MessageMedia(
+                fs.writeFile(
+                  pathFileNombrearchivo,
+                  respuestaTextodeDireccion,
+                  (errorescrituraArchivo) => {
+                    console.log(errorescrituraArchivo);
+                  }
+                )
+              );
+              //envio el archivo del informe dandole un tiempo de espera
+              setTimeout(async () => {
+                var archivomedia = MessageMedia.fromFilePath(
+                  `./informes/${nombreArchivomedia}`
+                );
+                await cliente.sendMessage(numeroEmisor, archivomedia);
+                //ahora borro el achivo generado
+                await fs.unlinkSync(pathFileNombrearchivo);
+              }, 10000);
+            } else {
+              cliente.sendMessage(
+                numeroEmisor,
+                "Estudiante no existe, verifique los datos y reintente. Si el problema persiste escriba a dcornejo@liceotecnicotalcahuano.cl indicando su rut, nombre y curso"
+              );
+            }
+          })
+          .catch((errorDireccionObtenida) => {
+            console.log(
+              "error de direccion obtenida url porque: " +
+                errorDireccionObtenida
+            );
+          });
+      })
+      .catch((errorApiNotas) => {
+        console.log("error en la api de notas porque: " + errorApiNotas);
+      });
+    //cliente.sendMessage(numeroEmisor,apirespuestafinal.toString());
+  } else if (cuerpoMensaje.toLowerCase().search(/boton/) >= 0) {
+    mensajeEntrante.reply("quieres boton"); //responder
+    setTimeout(async () => {
+      var boton = new Buttons("mensaje", [{ body: "cuerpo" }]);
+      console.log(boton.buttons);
+      await cliente.sendMessage(numeroEmisor, boton);
+      cliente.sendMessage(numeroEmisor, "te mande boton");
+    }, 10000);
+  } else if (cuerpoMensaje.toLowerCase().search(/adios/) >= 0) {
+    mensajeEntrante.reply(
+      "Chao. Para mas información visita cuando quieras https://www.profedaniel.cf"
+    );
+  } else {
+    //mensajeCleverbot=clever(mensajeEntrante.body.toLowerCase()).then(respuestaclever);
+    //console.log(mensajeCleverbot);
+    //cliente.sendMessage(mensajeEntrante.from,"no dijiste hola");
 
-})
+    /**contesta cleverbot */
+    clever(cuerpoMensaje)
+      .then(async (respuestacleverBot) => {
+        await console.log("respuesta cleverbot: " + respuestacleverBot);
+        cliente.sendMessage(numeroEmisor, respuestacleverBot);
+      })
+      .catch((errorCleverbot) => {
+        console.log(errorCleverbot);
+        cliente.sendMessage(
+          numeroEmisor,
+          `Por el momento tengo problemas para responder. escribeme mas tarde ${nombreNotificacion}`
+        );
+      });
+  }
+});
 
 cliente.initialize();
 
