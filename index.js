db179790875dfe8ba2f039bd86eb499b4f093988
daniel <ryu51474@gmail.com>
2022-09-18 23:49:41 -0300

    commit de correccion de bug de acentos. version final local

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ cliente.on("auth_failure", (errorAutenticacion) => {
 
 //para recibir y escuchar los mensajes entrantes
 cliente.on("message", (mensajeEntrante) => {//procesos de respuestas segun mensajes
-  let cuerpoMensaje = mensajeEntrante.body;
+  let cuerpoMensaje = mensajeEntrante.body.toLowerCase();
   console.log(mensajeEntrante.body);
   let nombreNotificacion = mensajeEntrante._data.notifyName;
   (nombreNotificacion.toLowerCase().search(/</)>=0)?nombreNotificacion='Estimado estudiante':
@@ -59,7 +59,7 @@ cliente.on("message", (mensajeEntrante) => {//procesos de respuestas segun mensa
 
   var ahora = new Date(); //PROCESO PENDIENTE: se ha subido aqui, sacado del primer if porque solo debe responder el bot si es muy tarde
 
-  if (cuerpoMensaje.toLowerCase().search(/hola/) >= 0) {//si el mensaje viene con la palabra hola responde un saludo al azar
+  if (cuerpoMensaje.search(/hola/) >= 0) {//si el mensaje viene con la palabra hola responde un saludo al azar
     var arrayRespuestas = [
       `estas bien?, un gusto saludarte ${nombreNotificacion}`,
       `son las ${ahora.getHours()}:${ahora.getMinutes()} en este momento, en serio me escribes a esta hora ${nombreNotificacion}?`,
@@ -72,36 +72,36 @@ cliente.on("message", (mensajeEntrante) => {//procesos de respuestas segun mensa
     cliente.sendMessage(mensajeEntrante.from, mensajeRespuestaSaludoAzar);
     cliente.sendMessage(numeroEmisor,`Si deseas saber que puedo hacer por ti puedes escribir **opciones** para saberlo`)
     console.log(mensajeRespuestaSaludoAzar);
-  } else if (cuerpoMensaje.toLowerCase().search(/nota/) >= 0) {//si en el mensaje existe la palabra nota da instrucciones para recibir notas
+  } else if (cuerpoMensaje.search(/nota/) >= 0) {//si en el mensaje existe la palabra nota da instrucciones para recibir notas
     cliente.sendMessage(
       numeroEmisor,
       `${nombreNotificacion}, si deseas saber notas debes de ahora ingresar solo tu rut, sin puntos ni guión, en caso de terminar en k reemplácelo con un 1, ej: el rut 12.345.678-k se escribe 123456781. si eres extranjero, no escribas el 100. SI NO LO HACE CORRECTAMENTE SU PETICION SERA ANULADA E IGNORADA (Puede que se responda con cualquier cosa absurda)`
     );
   } else if (!isNaN(cuerpoMensaje)&&validaRut(cuerpoMensaje)) {//envio de notas usando solo el rut
     envioNotas(cliente,nombreNotificacion,numeroEmisor,cuerpoMensaje);
-  } else if (cuerpoMensaje.toLowerCase().search(/boton/) >= 0) {//podria usarse aqui para un menu con la palabra ayuda
+  } else if (cuerpoMensaje.search(/boton/) >= 0) {//podria usarse aqui para un menu con la palabra ayuda
     mensajeEntrante.reply("quieres boton"); //responderal mensaje
     let boton=new Buttons('mensajeboton',[{body:'cuerpoboton'},{body:'cuerpoboton2'}],'titulo','footer');
     console.log(boton)
     mensajeEntrante.reply(boton)
     cliente.sendMessage(numeroEmisor,boton)//********falla opcion de envio, revisar */
     cliente.sendMessage(numeroEmisor,"mande botones")
-  } else if (cuerpoMensaje.toLowerCase().search(/lista/)>=0) {//envio de lista de itemes. parece no funcionar
+  } else if (cuerpoMensaje.search(/lista/)>=0) {//envio de lista de itemes. parece no funcionar
     console.log('inicio de proceso de lista')
     let secciones =[{title:'secciontitulo',rows:[{title:'item1',description:'descripcion'},{title:'item2'}]}];
     let lista = new List('cuerpo de lista','btnText',secciones,'titulo','footer');
     cliente.sendMessage(numeroEmisor,lista);
     console.log('fin proceso de lista')
-  } else if (cuerpoMensaje.toLowerCase().search(/adios/) >= 0) {//despedida con mensaje final
+  } else if (cuerpoMensaje.normalize("NFD").replace(/[\u0300-\u036f]/g, "").search(/adios/) >= 0) {//despedida con mensaje final
     mensajeEntrante.reply(
       "Chao. Para mas información visita cuando quieras https://www.profedaniel.cf"
     );
-  } else if (cuerpoMensaje.toLowerCase().search(/email/)>=0){//instrucciones de cambio de email en la base de datos
+  } else if (cuerpoMensaje.search(/email/)>=0){//instrucciones de cambio de email en la base de datos
     console.log('inicio de envio de  INSTRUCCIONES DE  cambio de email');
     cliente.sendMessage(numeroEmisor,`${nombreNotificacion}, para cambiar tu email en el que recibes las notas debes escribir ahora tu rut sin puntos ni guion seguido de una coma y el nuevo email. SIN ESPACIOS o su solicitud será rechazada. En caso que su rut termine en k reemplácelo por un 1. Si es extranjero no escriba el 100 \n ej: 123456781,nuevocorreo@gmail.com`)
-  } else if(cuerpoMensaje.toLowerCase().search(/@/)>=0){
+  } else if(cuerpoMensaje.search(/@/)>=0){
     //se analiza si esta correcto el mensaje
-    let rutconEmail = cuerpoMensaje.toLowerCase().split(',')
+    let rutconEmail = cuerpoMensaje.split(',')
     //regex del rut
     let RUT = rutconEmail[0].replace(/[\.,-]/g, "");
     console.log(RUT);
@@ -111,7 +111,7 @@ cliente.on("message", (mensajeEntrante) => {//procesos de respuestas segun mensa
       console.log(`${nuevoEmailalumno} es un email valido`);
       cambioEmail(cliente,nombreNotificacion,numeroEmisor,cuerpoMensaje);
     }
-  } else if (cuerpoMensaje.toLowerCase().search(/opciones/)>=0){//opciones del bot y sus acciones
+  } else if (cuerpoMensaje.search(/opciones/)>=0){//opciones del bot y sus acciones
     cliente.sendMessage(numeroEmisor,`Opciones: ${nombreNotificacion} escribe en palabras tu solicitud segun lo que quieras hacer\n`+
                                       '1.- escribe **opciones** para volver a ver este mensaje\n'+
                                       '2.- puedes **pedir tus notas** simplemente escribiéndolo\n'+
